2eaf94d1a880d1a38ac37af4f578c1cdbf93fc9e
dosaem <dosaem90@gmail.com>
2023-05-19 17:05:20 +0900

    docs: step2 로또 구현할 기능 정리

---
+++ src/main/java/step2/LottoApplication.java
@@ -0,0 +1,10 @@
+package step2;
+
+import step2.view.InputView;
+
+public class LottoApplication {
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        inputView.buyingLotto();
+    }
+}
